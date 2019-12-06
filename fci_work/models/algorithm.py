08d27e22966c51b0a0f2d44807168f632b4403d8
Mostafa <mail@most>
2019-12-07 01:50:29 +0200

    [ADD] update prime algorithm & image resoluation

--- fci_work/models/algorithm.py
+++ fci_work/models/algorithm.py
@@ -5,7 +5,8 @@ import itertools
 import random
 from collections import defaultdict
 from heapq import *
-
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import networkx as nx
 
